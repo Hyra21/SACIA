8157a93cc62a115ce1b610f3d9f27deeddc67349
Hyra21 <yaed.21@outlook.es>
2023-05-16 19:08:29 -0700

    Version con navigation incluido.

--- app/src/main/java/com/alucintech/saci/MainActivity.java
+++ app/src/main/java/com/alucintech/saci/MainActivity.java
@@ -15,51 +15,14 @@ import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView text, errorText;
-    Button show;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text=(TextView) findViewById(R.id.textTest);
-        errorText=(TextView) findViewById(R.id.textError);
-        show=(Button) findViewById(R.id.buttonTest);
 
-        show.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                new Task().execute();
-            }
-        });
     }
 
-    class Task extends AsyncTask<Void, Void, Void> {
-        String records="", error="";
 
-        @Override
-        protected Void doInBackground(Void... voids){
-            try{
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.1.69:3307/sacibd","HYRA99","root");
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM actividad;");
-
-                while(resultSet.next()){
-                    records += resultSet.getString(1) + " " + resultSet.getString(2) + "\n";
-                }
-            } catch (Exception e){
-                error = e.toString();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            text.setText(records);
-            if(error !="")
-                errorText.setText(error);
-            super.onPostExecute(aVoid);
-        }
-    }
 }
