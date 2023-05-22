6df140681be7e00a0688373d435c1a73fe6a605e
Hyra21 <yaed.21@outlook.es>
2023-05-22 16:44:53 -0700

    1.2

--- app/src/main/java/com/alucintech/saci/ConsultaCarnet.java
+++ app/src/main/java/com/alucintech/saci/ConsultaCarnet.java
@@ -1,6 +1,7 @@
 package com.alucintech.saci;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -26,11 +27,37 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class ConsultaCarnet extends Fragment {
 
     ImageButton btnRegresar, btnScanner, btnMenu;
     NavController navController;
 
+    class Task extends AsyncTask<View, View, View>{
+        //Aquí subimos todos los datos a la base de datos
+        @Override
+        protected View doInBackground(View... views) {
+            try{
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.1.69:3307/sacibd", "HYRA99", "root");
+                Statement statement = connection.createStatement();
+
+                ResultSet resultSet = statement.executeQuery("");
+            } catch (Exception e){
+
+            }
+            return null;
+        }
+        //Aquí crearemos la vista de los carnets del alumno
+        @Override
+        protected void onPostExecute(View view) {
+            super.onPostExecute(view);
+        }
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -42,6 +69,8 @@ public class ConsultaCarnet extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         btnRegresar = view.findViewById(R.id.imgbtRegresar);
         btnScanner = view.findViewById(R.id.imgbtScanner);
+
+        new Task().execute();
 
         btnRegresar.setOnClickListener(new View.OnClickListener() {
             @Override
